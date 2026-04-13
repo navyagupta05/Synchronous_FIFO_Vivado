817566d7254dd08cce4c3a841a9500f4455a3507
navyagupta05 <145274264+navyagupta05@users.noreply.github.com>
2026-04-13 20:34:47 +0530

    Update and rename sync_FIFO.v to src/sync_FIFO.v

--- src/sync_FIFO.v
+++ src/sync_FIFO.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/02/2026 03:27:09 PM
-// Design Name: 
-// Module Name: sync_FIFO
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module sync_FIFO
     // Parameters
